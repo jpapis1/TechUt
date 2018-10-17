8462ddd2bebfdfff08461a211ad956ecff408e5f
jpapis1 <jpapis1@gmail.com>
2018-10-17 17:06:47 +0200

    Added zad02

---
+++ zad02/src/main/java/app/Main.java
@@ -0,0 +1,9 @@
+package app;
+
+import app.service.ParrotService;
+
+public class Main {
+    public static void main(String[] args) {
+        ParrotService parrotService = new ParrotService();
+    }
+}
---
+++ zad02/src/main/java/app/domain/Parrot.java
@@ -0,0 +1,9 @@
+package app.domain;
+
+public class Parrot {
+    private long id;
+    private String latinName;
+    private String givenName;
+    private int age;
+    private String countryOfOrigin;
+}
